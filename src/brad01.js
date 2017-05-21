dab3de09f79275b3c5ee9b4c7c29ea5b65354c16
jack000159 <jack000159@gmail.com>
2017-05-21 11:10:27 +0800

    20170521v1

--- src/brad01.js
+++ src/brad01.js
@@ -3,25 +3,6 @@ var Brad01Layer = cc.Layer.extend({
         this._super();
         var size = cc.winSize;
 
-        var colorLayer = new cc.LayerColor(
-            cc.color(255,255,0),
-            192,55
-        );
-        colorLayer.x = size.width / 2;
-        colorLayer.y = size.height / 2;
-        colorLayer.ignoreAnchorPointForPosition(false);
-        this.addChild(colorLayer,0);
-
-        var mylabel = new cc.LabelTTF("我的遊戲","Arial",48);
-        var labelSize = mylabel.getContentSize();
-        cc.log(labelSize.width + "x" + labelSize.height);
-        mylabel.x = size.width / 2;
-        mylabel.y = size.height / 2;
-        mylabel.setColor(cc.color(0,0,255));
-        this.addChild(mylabel,0);
-
-        mylabel.setString("我的遊戲v2");
-
         var n8 = new cc.Sprite("res/imgs/number8.png");
         n8.attr({
             x: size.width / 2,
@@ -36,22 +17,49 @@ var Brad01Layer = cc.Layer.extend({
         });
         this.addChild(n7);
 
+        var n0 = new cc.Sprite(res.number_png,cc.rect(1,1,70,70));
+        n0.attr({
+            x: size.width / 2,
+            y: size.height * 2 / 4
+        });
+        this.addChild(n0);
 
+        var frameCache = cc.spriteFrameCache;
+        frameCache.addSpriteFrames(
+            res.number_plist, res.number_png);
 
+        var n1 = new cc.Sprite("#number0.png");
+        n1.attr({
+            x: size.width * 3 / 4,
+            y: size.height * 2 / 4
+        });
+        this.addChild(n1);
+
+        this.MyMouseListener(this);
 
         return true;
     },
+
+
+    // 定義一個自訂方法名稱: MyMouseListener
+    MyMouseListener: function(layer){
+        if ('mouse' in cc.sys.capabilities){
+            // mouse listener
+            var mouseListener = {
+                event: cc.EventListener.MOUSE,
+                onMouseDown: function(event){
+                    cc.log("down");
+                }
+            };
+            cc.eventManager.addListener(mouseListener,this);
+
+        }
+    }
 });
 
 var Brad01Scene = cc.Scene.extend({
-    ctor:function () {
-        this._super();
-        cc.log("A");
-        cc.warn("A");
-        cc.error("A");
-    },
     onEnter:function () {
-        cc.log("B");
+        this._super();
         var layer = new Brad01Layer();
         this.addChild(layer);
     }
