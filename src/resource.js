49f52b08803d5256c51873881ade7d9ff4f22435
jack000159 <jack000159@gmail.com>
2017-06-04 14:44:40 +0800

    20170604v3

---
+++ src/resource.js
@@ -0,0 +1,15 @@
+var res = {
+    HelloWorld_png : "res/HelloWorld.png",
+    number7_png : "res/imgs/number7.png",
+    back_png : "res/back.png",
+    enter_png : "res/enter.png",
+    winner_png : "res/winner.png",
+    loser_png : "res/loser.png",
+    number_png : "res/number.png",
+    number_plist : "res/number.plist",
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
